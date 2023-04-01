085ef2155a9769767f652355f8f9ac1a35bea4ce
Ankush <ankushguitar@gmail.com>
2023-04-01 20:29:58 +0530

    final result

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("HELO");
+const modalButton = document.getElementById("modal-button");
+const modal = document.getElementById("modal");
+const closeButton = document.getElementById("close");
+
+modalButton.addEventListener("click", () => {
+  modal.classList.remove("hide");
+  modalButton.classList.add("hide");
+});
+
+closeButton.addEventListener("click", () => {
+  modal.classList.add("hide");
+  modalButton.classList.remove("hide");
+});
